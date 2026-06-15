a05d84127ef11c703c71c469327eab30cfebb892
Andrei Miscenco <mapriotii@gmail.com>
2026-06-15 17:34:19 +0300

    add PostgreSQL service

--- app/core/config.py
+++ app/core/config.py
@@ -1,14 +1,17 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+
 class Settings(BaseSettings):
-    app_name: str = 'Highload Ticket Booking API'
-    app_env: str = 'local'
+    app_name: str = "Highload Ticket Booking API"
+    app_env: str = "local"
     app_debug: bool = True
+    database_url: str
 
     model_config = SettingsConfigDict(
         env_file=".env",
         env_file_encoding="utf-8",
         extra="ignore",
     )
+
 
 settings = Settings()
